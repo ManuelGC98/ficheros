1354d49ce63997110ea545d8f512c6ee6efe1df1
formacion <formacion@192.168.61.133>
2018-05-15 08:57:47 +0200

    commit 2

--- src/main/java/es/dual/ficheros/ficheros/Ficheros.java
+++ src/main/java/es/dual/ficheros/ficheros/Ficheros.java
@@ -1,6 +1,10 @@
 package es.dual.ficheros.ficheros;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.security.SecureRandom;
+
+import org.omg.CORBA_2_3.portable.OutputStream;
 
 public class Ficheros {
 	public static void main(String[] args) {
@@ -10,13 +14,31 @@ public class Ficheros {
 
 		File directorio = new File(ruta);
 
-		for (String fichero : directorio.list()) {
+		// 2. Crear un fichero con nombre "file_" + RANDOM + ".txt" y rellenar con contenido aleatorio
+		SecureRandom random = new SecureRandom();
+		
+		OutputStream out;
+		try
+		{
+			out = new FileOutputStream(new File(ruta + "file_" + random.nextLong() + ".txt"));
+			StringBuffer datos = new StringBuffer();
+			datos.append(random.nextLong() + " " + random.nextLong() + " " + random.nextLong());
+			out.write(datos.toString().getBytes());
+			out.close();
+		}
+		catch (Throwable e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		/*for (String fichero : directorio.list()) {
 			File temp = new File(ruta + "/" + fichero);
 
 			if (!temp.isDirectory()) {
 				System.out.println(fichero);
 			}
-		}
+		}*/
 
 	}
 }
